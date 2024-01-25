e3f9843d2c9be88caaf55a96558214cb5464dd5e
mrgb <syahidnurrohim@gmail.com>
2024-01-25 16:30:08 +0700

    feat: update url

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,4 +1,6 @@
+import { isBrowser } from "./is-browser";
+
 export const getBaseURL = (path = "") => {
-  const baseURL = window.location.origin;
+  const baseURL = isBrowser() ? window.location.origin : process.env.BASE_URL;
   return new URL(path, baseURL).toString();
 };
